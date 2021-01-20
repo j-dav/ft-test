8cec91058d53e9ec007bf485272fdc3e0b7ec0f6
j-dav <hybridbad90@gmail.com>
2021-01-20 11:47:09 +0000

    chore(lint + jest): add eslint and jest sample

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ft-test
+
+Set up linting using eslint --init and used article below
+
+https://medium.com/dwarves-foundation/automatically-lint-prettify-your-javascript-project-using-husky-lint-staged-cae8e685bb06
+
+https://medium.com/@pojotorshemi/integration-test-on-express-restful-apis-using-jest-and-supertest-4cf5d1414ab0
--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
+/* eslint-disable no-console */
+require('dotenv').config()
 
-const app = express();
-const port = process.env.PORT || 3000;
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 3000
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+app.use((req, res, next) => {
+  next(createError(404))
+})
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 app.listen(port, () => {
-  if (process.env.NODE_ENV !== "test") {
-    console.log(`FT app listening on port ${port}!`);
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(`FT app listening on port ${port}!`)
   }
-});
+})
 
-module.exports = app;
+module.exports = app
