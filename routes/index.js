a2674cc965a19133f04a9030ac8fc5ca7478bd2b
James Davies <hybridbad90@gmail.com>
2021-01-21 21:43:47 +0000

    feat(headline): headline styling

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("search", { layout: "default" });
+  res.render("index", { layout: "default" });
 });
 
 module.exports = router;
